93b8991437d60f94d7de7b943de74747602119cd
Jon Boone <ipmonger@delamancha.org>
2026-09-03 16:59:55 -0400

    updated to handle frame refresh

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt: float = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player_1.update(dt)
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
