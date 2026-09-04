721ca309fecf92deb1bb851f14d6f72ad4ab22c5
Jon Boone <ipmonger@delamancha.org>
2026-09-04 05:46:30 -0400

    added group support for update() and draw()

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt: float = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    updatable.add(player_1)
+    drawable.add(player_1)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player_1.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player_1.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
